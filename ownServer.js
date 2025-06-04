c079c066a68e308c1c59c9cdc01f9c656baacd9e
Ankit Raj <ankitrajcollab@gmail.com>
2025-06-04 14:28:28 +0530

    create a basic node js server

--- ownServer.js
+++ ownServer.js
@@ -1,9 +1,9 @@
-import express from "express";
+const express = require("express");
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Welcome to My Hotel ... How i can help you ! ");
 });
 
-app.listen(3000);
+app.listen(3000); // localhost me haamre application ke liye ek room number allot hua hai
