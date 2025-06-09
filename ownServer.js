aea3e2d7568b9b8fa48c01a2be880704bae755f4
Ankit Raj <ankitrajcollab@gmail.com>
2025-06-09 18:17:16 +0530

    add post method to save person data

--- ownServer.js
+++ ownServer.js
@@ -18,9 +18,35 @@ app.get("/", (req, res) => {
   res.send("Welcome to My Hotel ... How can I help you!");
 });
 
+app.post("/person", (req, res) => {
+  const data = req.body; // Assuming the body contains the person data
+  const newPerson = new Person(data);
+  // newPerson.name = data.name;
+  // newPerson.age = data.age;
+  // newPerson.mobile = data.mobile;
+  // newPerson.email = data.email;
+  // newPerson.salary = data.salary;
+  // newPerson.age = data.salary;
+  // newPerson.address = data.address;
+
+  newPerson.save((error, savedPerson) => {
+    if (error) {
+      console.log("Error saving person: ", error);
+      res.status(500).json({
+        error: "Internal Server Error",
+      });
+    }else{
+      console.log('data saved successfully');
+      res.status(200).json(savedPerson)
+    }
+  });
+});
+
 // Start the server and listen on port 3000
 app.listen(3000, () => {
   console.log("Listening on Port 3000");
 });
 
 // Note: Port 3000 is like a "room number" assigned to our application on localhost
+
+// 38:11
